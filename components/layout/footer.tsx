8093d7f0323a6bc6dea095fbb867d2ceae192036
Rahul Patel <ra.rahul11131@gmail.com>
2025-03-27 22:10:52 +0530

    Change Link

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -182,7 +182,7 @@ export default function Footer() {
               </div>
               <div className="mt-6">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.2536900776364!2d77.20659841508096!3d28.56325198244407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce26f903969d7%3A0x8367180c6de2ecc2!2sAIIMS%20Delhi!5e0!3m2!1sen!2sin!4v1647870173809!5m2!1sen!2sin"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.2977121367485!2d81.08050667529416!3d26.89404517665833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39995f2f5b8b9591%3A0xbd9e770717a687c0!2sGoel%20Group%20of%20Institutions%2C%20Lucknow!5e0!3m2!1sen!2sin!4v1743010650457!5m2!1sen!2sin"
                   width="100%"
                   height="150"
                   style={{ border: 0 }}
